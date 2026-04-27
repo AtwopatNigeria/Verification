20a9a61b4832e09ba63de039475536591b34d087
AtwopatNigeria <support.atwopat@gmail.com>
2026-04-27 21:27:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzsFfOF0p_u7O2TkkR1AhMuWnjvNKZOOyi-cuD0uDURnYQt5CQaNDfrILz7Da86yMzYUA/exe";
-
-
-
-**/ "https://script.google.com/macros/s/AKfycbzsFfOF0p_u7O2TkkR1AhMuWnjvNKZOOyi-cuD0uDURnYQt5CQaNDfrILz7Da86yMzYUA/exec"; /**
+const API_URL = "https://script.google.com/macros/s/AKfycbzsFfOF0p_u7O2TkkR1AhMuWnjvNKZOOyi-cuD0uDURnYQt5CQaNDfrILz7Da86yMzYUA/exec";
